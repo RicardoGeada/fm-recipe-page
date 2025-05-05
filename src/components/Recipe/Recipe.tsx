2f5394905d10b974e39758b9482d3bc8bbab221c
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-05-05 10:11:02 +0200

    feat(recipe): add ingredients section

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -38,6 +38,21 @@ function Recipe() {
             </li>
           </ul>
         </section>
+
+        {/* ingredients */}
+        <section className={styles["recipe__ingredients"]}>
+          <h2>Ingredients</h2>
+          <ul>
+            <li>2-3 large eggs</li>
+            <li>Salt, to taste</li>
+            <li>Pepper, to taste</li>
+            <li>1 tablespoon of butter or oil</li>
+            <li>Optional fillings: cheese, diced vegetables, cooked meats, herbs</li>
+          </ul>
+        </section>
+
+        {/* line */}
+        <div className="line--h"></div>
       </div>
     </div>
   );
