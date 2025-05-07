f9d4168189b7da1ef3c45f71c21593ad91f9a63b
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-05-07 13:52:26 +0200

    refactor(data): move recipe content to separate recipe.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,14 @@
 import "./App.scss";
 import Recipe from "./components/Recipe/Recipe";
+import recipe_data from "./data/recipe.json";
 
 function App() {
-  const recipe = {
-    title: "Simple Omelette Recipe",
-    description:
-      "An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.",
-    image: "image-omelette.jpeg",
-    preparationTime: {
-      total: "Approximately 10 minutes",
-      prep: "5 minutes",
-      cook: "5 minutes",
-    },
-    ingredients: [
-      "2-3 large eggs",
-      "Salt, to taste",
-      "Pepper, to taste",
-      "1 tablespoon of butter or oil",
-      "Optional fillings: cheese, diced vegetables, cooked meats, herbs",
-    ],
-    instructions: [
-      {
-        title: "Beat the eggs",
-        description:
-          "In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.",
-      },
-      {
-        title: "Heat the pan",
-        description:
-          "Place a non-stick frying pan over medium heat and add butter or oil.",
-      },
-      {
-        title: "Cook the omelette",
-        description:
-          "Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.",
-      },
-      {
-        title: "Add fillings (optional)",
-        description:
-          "When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.",
-      },
-      {
-        title: "Fold and serve",
-        description:
-          "As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.",
-      },
-      {
-        title: "Enjoy",
-        description: "Serve hot, with additional salt and pepper if needed.",
-      },
-    ],
-    nutrition: {
-      calories: "277",
-      carbs: "0",
-      protein: "20",
-      fat: "22",
-    },
-  };
+
 
   return (
     <>
       <main>
-        <Recipe recipe={recipe} />
+        <Recipe recipe={recipe_data} />
       </main>
     </>
   );
